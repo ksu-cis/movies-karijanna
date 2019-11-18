52cb54b2e35f6de4eca348b7bd1c9bb3ef5d917d
Karijanna Miller <narae7595@ksu.edu>
2019-11-18 15:18:58 -0600

    Commit

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
-
 namespace Movies
 {
     /// <summary>
@@ -13,12 +12,12 @@ namespace Movies
     public class MovieDatabase
     {
         private List<Movie> movies = new List<Movie>();
-
         /// <summary>
         /// Loads the movie database from the JSON file
         /// </summary>
-        public MovieDatabase() {
-            
+        public MovieDatabase()
+        {
+
             using (StreamReader file = System.IO.File.OpenText("movies.json"))
             {
                 string json = file.ReadToEnd();
@@ -27,5 +26,46 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            // Case 0: No search string, no ratings
+            if (searchString == null && rating.Count == 0) return All;
+            List<Movie> results = new List<Movie>();
+
+            foreach (Movie movie in movies)
+            {
+                // Case 1: Search string AND ratings
+                if (searchString != null && rating.Count > 0)
+                {
+                    if (movie.Title != null
+                        && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                        && rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+                }
+
+                // Case 2: Search string only
+                else if (searchString != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        results.Add(movie);
+                    }
+                }
+
+                // Case 3: Ratings only
+                else if (rating.Count > 0)
+                {
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+                }
+            }
+            return results;
+
+        }
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        MovieDatabase movieDatabase = new MovieDatabase();
+
+        public List<Movie> Movies;
 
         public void OnGet()
         {
 
+            Movies = movieDatabase.All;
+        }
+
+        public void OnPost(string search, List<string> rating)
+        {
+
+            Movies = movieDatabase.SearchAndFilter(search, rating);
         }
     }
 }
